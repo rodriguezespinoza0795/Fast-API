06b898b18333b3b2682211afec05a311e99e4f0f
David Rodriguez <rodriguezespinoza0795@gmail.com>
2021-10-21 11:44:24 -0500

    First Model

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+from fastapi import FastAPI,Body
 
 app = FastAPI()
+
+class User(BaseModel):
+    id: int
+    name: str
+    email: str
+    address: Optional[str] = None
+    is_active: bool
+
 
 @app.get("/")
 def home():
     return {"Hello":"World"}
 
+@app.post("/user/new")
+def create_user(user:User = Body(...)):
+    return user
