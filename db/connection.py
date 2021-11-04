6911008a2341e51abd292cc2e72603283d5d6cb6
David Rodriguez <rodriguezespinoza0795@gmail.com>
2021-11-03 18:21:22 -0600

    insert_table

--- db/connection.py
+++ db/connection.py
@@ -3,10 +3,9 @@ import pandas as pd
 from datetime import datetime
 
 def bbox_connect():
-    database_type = 'mysql'
-    user = 
-    password =
-    host = 
+    user = ''
+    password = ''
+    host = ''
     database = 'bBox'
     conn_string = 'mysql+pymysql://{}:{}@{}/{}'.format(user, password, host, database)
     sql_conn = sql.create_engine(conn_string)
@@ -24,11 +23,13 @@ def conn_close(sql_conn):
     connection = sql_conn.raw_connection()
     connection.close()
 
-header = ['CAE','LOGIN_ID','Extn','logged_in', 'login', 'logged_out', 'logout', 'logout_date', 'skill1', 'skill2', 'skill3', 'skill4', 'skill5', 'skill6', 'skill7', 'skill8', 'skill9']
-df = pd.read_csv('./../login_logout.txt',names=header, skiprows=2, delimiter=';')
-df['logout_date'].ffill(inplace=True)
-#pd.to_datetime(df['logout_date'], format='%d/%m/%y').dt.strftime('%Y-%m-%d')
-#print(df[['LOGIN_ID', 'login', 'logout', 'logout_date']])
-
-
-print(datetime.datetime.strptime(df['logout_date'], '%Y-%m-%d').strftime('%m/%d/%y'))
+def load_adh_report():
+    header = ['CAE','LOGIN_ID','Extn','logged_in', 'login', 'logged_out', 'logout', 'logout_date', 'skill1', 'skill2', 'skill3', 'skill4', 'skill5', 'skill6', 'skill7', 'skill8', 'skill9']
+    df = pd.read_csv('login_logout.txt',names=header, skiprows=2, delimiter=';', parse_dates=['logout_date'])
+    df['logout_date'].ffill(inplace=True)
+    df['login'] = pd.to_datetime(df['login']).dt.strftime('%H:%M:%S')
+    df['logout'] = pd.to_datetime(df['logout'], errors='coerce').dt.strftime('%H:%M:%S')
+    df =df[['LOGIN_ID', 'login', 'logout', 'logout_date']]
+    engine = bbox_connect()
+    df.to_sql('tbl_adh_report_temp', con = engine, if_exists = 'replace')
+    conn_close(engine)
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from fastapi import FastAPI
 from db import connection as sql
 from fastapi.middleware.cors import CORSMiddleware
@@ -15,60 +14,13 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-=======
-from typing import Optional
-from pydantic import BaseModel
-from fastapi import FastAPI,Body,Query, Path
-
-app = FastAPI()
-
-class User(BaseModel):
-    id: int
-    name: str
-    email: str
-    address: Optional[str] = None
-    is_active: bool
-
->>>>>>> a779270a29e0a65ad34a3664ff287254b30e74e3
 
 @app.get("/")
 def home():
+    sql.load_adh_report()
     bbox = sql.bbox_connect()
-    query = 'select * from e_employee where id_position=73 and is_active=1;'
+    query = "call sp_write_cms_inout(5,'','',0);"
     results = sql.plane_query_text(bbox, query)
     sql.conn_close(bbox)
-
-<<<<<<< HEAD
     return results
-=======
-@app.post("/user/new")
-def create_user(user:User = Body(...)):
-    return user
-
-@app.get("/person/detail")
-def show_person(
-    name: Optional[str] = Query(
-        None, 
-        min_length=1, 
-        max_length=50,
-        title="Username",
-        description="This is the user name. It's between 1 and 50 characters"
-        ),
-    age: int = Query(
-        ...,
-        title="User age",
-        description="This is the user age. It's required"
-        )
-    ): 
-    return {name: age}
-
-@app.get("/person/detail/{id}")
-def show_person(id: int = Path(
-    ..., 
-    gt=0,
-    title="User id",
-    description="This is the user id. It's required"
-    )
-): 
-    return {id: "It exists!"}
->>>>>>> a779270a29e0a65ad34a3664ff287254b30e74e3
+    
